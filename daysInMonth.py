17864ca845824f2e0db669943df934b1331155c7
lsabala <121403210+lsabala@users.noreply.github.com>
2023-01-05 15:11:10 -0500

    Add files via upload

---
+++ daysInMonth.py
@@ -0,0 +1,48 @@
+#DAYS IN MONTH----------------------------------------------------------------------
+#First, convert this function is_leap() so that instead of printing "Leap year." or "Not leap year." it should return True if it is a leap year and return False if it is not a leap year.
+#You are then going to create a function called days_in_month() which will take a year and a month as inputs, e.g.
+  #days_in_month(year=2022, month=2)
+
+
+#days_in_month(year=2022, month=2)
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False 
+
+def days_in_month(year, month):
+  #Non Leap year
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+ 
+  #And it will use this information to work out the number of days in the month, then return that as the output, e.g.:
+  #The List month_days contains the number of days in a month from January to December for a non-leap year. A leap year has 29 days in February.'''
+  value = is_leap(year)
+  if value == True and month == 2:
+    return 29
+  elif value == True and month != 2:
+    return month_days[month - 1]
+  elif value == False:
+    return month_days[month - 1]
+    
+
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+
+
